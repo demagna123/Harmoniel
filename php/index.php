b85831029625e5c52f73805322060296942838ba
demagna123 <demagnanestor@gmail.com>
2025-02-18 19:57:31 +0100

    second commit

---
+++ php/index.php
@@ -0,0 +1,193 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <!-- Header -->
+    <?php
+include 'navbar.php';
+?>
+
+    <!-- carousel -->
+    <div class="carousel">
+      <!-- list item -->
+      <div class="list">
+        <div class="item">
+          <img src="../image/backgroud1.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <!-- <div class="topic">ANIMAL</div> -->
+            <div class="des">
+              <!-- lorem 50 -->
+              Prenez soins de vous naturellement avec
+               nos produits menstruelles
+              réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <!-- <div class="topic">ANIMAL</div> -->
+
+            <div class="des">
+              <!-- lorem 50 -->
+              Prenez soins de vous naturellement avec 
+              nos produits menstruelles
+              réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg2.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <div class="des">
+            Prenez soins de vous naturellement avec <br> 
+              nos produits menstruelles
+              réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg4.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <div class="des">
+            Prenez soins de vous naturellement avec  
+            nos produits menstruelles
+            réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg5.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <div class="des">
+            Prenez soins de vous naturellement avec 
+              nos produits menstruelles
+              réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg6.jpg" />
+          <div class="content">
+            <div class="author">Bienvenue sur Harmoniel</div>
+            <div class="title">
+              Eh les filles, Harmoniel est fait pour nous!
+            </div>
+            <div class="des">
+              Prenez soins de vous naturellement avec <br> 
+              nos produits menstruelles
+              réutilisabbles
+            </div>
+            <div class="buttons">
+              <button>Nos Produits</button>
+              <button>En Savoir Plus</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- list thumnail -->
+      <div class="thumbnail">
+        <div class="item">
+          <img src="../image/backgroud1.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg2.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg6.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg4.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+        <div class="item">
+          <img src="../image/bg5.jpg" />
+          <div class="content">
+            <div class="title">Name Slider</div>
+            <div class="description">Description</div>
+          </div>
+        </div>
+      </div>
+      <!-- next prev -->
+
+      <div class="arrows">
+        <button id="prev"><</button>
+        <button id="next">></button>
+      </div>
+      <!-- time running -->
+      <div class="time"></div>
+    </div>
+
+    <script src="script.js"></script>
+  </body>
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,19 @@
+
+
+<header>
+    <nav>
+       <div class="container">
+       <div class="logo">
+          <img src="../image/logo.jpeg" alt="" width="500px" height="500px" />
+        </div>
+        <ul class="menu">
+          <li><a href="#products">Nos produits</a></li>
+          <li><a href="#education">Éducation & Sensibilisation</a></li>
+          <li><a href="#about">À propos</a></li>
+          <li><a href="#testimonials">Témoignages</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+       </div>
+      </nav>
+   
+    </header>
